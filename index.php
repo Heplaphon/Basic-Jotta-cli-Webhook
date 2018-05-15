422a25dc57ec7f04ed9e8fdc9a695019b7d2b252
Eirik Furuseth <hepalphon@gmail.com>
2018-05-15 16:52:54 +0000

    Styling updates

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<head>
+<title>Jotta-cli webhook</title>
+</head>
+<center>
 <?php
-echo file_get_contents('data.json');
+$jesson =  file_get_contents('data.json');
+$jesson = json_decode($jesson);
+$titile = $jesson->text;
+$titile = str_replace("\n","<br>", $titile);
+echo $titile;
+echo"<br>";
+foreach($jesson->attachments as $uploads){
+    if ($uploads->fields != null){
+		echo "Current status <br>";
+		echo $uploads->fields[0]->title;
+		echo "<div style='height:13vh;width:13vw;background-color:".$uploads->color.";'></div>";
+    }
+}
 ?>
+</center>
